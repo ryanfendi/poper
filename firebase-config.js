10b3944bda4c06da5ea5b38bef01c20eca40f408
ryanfendi <102475046+ryanfendi@users.noreply.github.com>
2026-09-04 08:06:25 +0700

    Add Firebase configuration and initialization

---
+++ firebase-config.js
@@ -0,0 +1,40 @@
+import { initializeApp } from
+  "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+
+import { getAuth } from
+  "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+import { getFirestore } from
+  "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDTMJ1zqIMDNo23YWkA_CUlKhgWlD1P6bA",
+
+  authDomain:
+    "poperi.firebaseapp.com",
+
+  projectId:
+    "poperi",
+
+  storageBucket:
+    "poperi.firebasestorage.app",
+
+  messagingSenderId:
+    "505386473466",
+
+  appId:
+    "1:505386473466:web:8a122f2b4f73a232ea7643"
+};
+
+
+const app =
+  initializeApp(firebaseConfig);
+
+
+export const auth =
+  getAuth(app);
+
+export const db =
+  getFirestore(app);
